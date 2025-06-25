625fa56a9e4bb47b2c63d22b2369f85f64f28532
Maanux <emanuel.skanis@hotmail.com>
2025-06-24 21:44:51 -0300

    Auto-generated commit

--- server/main.server.mjs
+++ server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-BYAEJVJT.mjs";import"./chunk-AYRG6HTB.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-QFKAASB6.mjs";import"./chunk-HJOS4HIQ.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
